25cdc97352b41061865f52be613a13454878a9be
Min-byeonghyun <qudgus5125@naver.com>
2025-08-07 00:15:43 +0900

    Promise generic

---
+++ src/section5/chapter28.ts
@@ -0,0 +1,47 @@
+/**
+ * Promise
+ */
+// Promise<type> type정의
+const promise = new Promise<number>((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20);
+    // reject("~~때문에 실패");
+  }, 3000);
+});
+
+promise
+  .then((res) => {
+    console.log(res * 10); //20
+  })
+  .catch((err) => {
+    if (typeof err === "string") {
+      console.log(err);
+    }
+  });
+
+/**
+ * Promise를 반환하는 함수의 타입을 정의
+ */
+interface Post {
+  id: number;
+  title: string;
+  content: string;
+}
+
+function fetchPost(): Promise<Post> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        title: "게시글 제목",
+        content: "게시글 컨텐츠",
+      });
+    }, 3000);
+  });
+}
+
+const postData = fetchPost();
+
+postData.then((post) => {
+  post.id;
+});
