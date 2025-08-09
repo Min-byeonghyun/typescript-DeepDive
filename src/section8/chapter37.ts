d90de7e523722fbf9299c56d05ceca66c8905d3f
Min-byeonghyun <qudgus5125@naver.com>
2025-08-09 17:56:16 +0900

    유틸리티 타입 2 Pick, Omit, Record

---
+++ src/section8/chapter37.ts
@@ -0,0 +1,70 @@
+/**
+ * 맵드 유틸리티 타입 2
+ */
+
+/**
+ * Pick<T,K> -> 객체 타입으로부터 특정 프로퍼티만 골라내는 타입
+ * -> 뽑다 , 고르다
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  // K extends 'title' | 'tags' | 'content' | 'thumbnailURL'
+  // 'title' | 'content' extends  'title' | 'tags' | 'content' | 'thumbnailURL'
+  [key in K]: T[key];
+};
+
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "옛날 글",
+  content: "옛날 컨텐츠",
+};
+
+/**
+ * Omit<T,K> -> 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
+ * -> 생략하다, 뺴다
+ */
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+
+// T = Post, K = 'title'
+// Pick <Post, Exclude<keyof Post, 'title>>
+// Pick <Post, Exclude<'title' | 'content' | 'tags' | thumbnailURL, 'title>>
+// Pick<Post, 'content' | 'tags' | 'thumbnailURL'>
+
+const noTitlePost: Omit<Post, "title"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+/**
+ * Record<K,V>
+ * 객체 타입을 만들어주는 유틸리티 타입
+ */
+
+type ThumbnailLegacy = {
+  large: {
+    url: string;
+  };
+  medium: {
+    url: string;
+  };
+  small: {
+    url: string;
+  };
+  watch: {
+    url: string;
+  };
+};
+
+type Record<K extends keyof any, V> = {
+  [key in K]: V;
+};
+
+type Thumbnail = Record<"large" | "medium" | "small", { url: string }>;
